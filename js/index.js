937d0fc30366fdab6b8a6811bafeda4cc376892e
timand1 <80768929+timand1@users.noreply.github.com>
2022-01-31 14:22:28 +0100

    Functions done

--- js/index.js
+++ js/index.js
@@ -14,84 +14,144 @@ Du ska enbart kunna gissa på en bokstav i taget.
 Ordet får inte vara hårdkodat i Javascript-filen när den ska jämföras - words-array
 */
 
-//  För att toggla SVG:en
-//  document.querySelector('figure').classList.add('scaffold')
-//  document.querySelector('figure').classList.add('head')
-//  document.querySelector('figure').classList.add('body')
-//  document.querySelector('figure').classList.add('arms')
-//  document.querySelector('figure').classList.add('legs')
 
 function showHangman() {
     if(incorrectGuess == 1) {
-        document.querySelector('figure').classList.add('scaffold')
+        document.querySelector('figure').classList.add('scaffold');
     } else if(incorrectGuess == 2) {
-        document.querySelector('figure').classList.add('head')
+        document.querySelector('figure').classList.add('head');
     } else if(incorrectGuess == 3) {
-        document.querySelector('figure').classList.add('body')
+        document.querySelector('figure').classList.add('body');
     } else if(incorrectGuess == 4) {
-        document.querySelector('figure').classList.add('arms')
+        document.querySelector('figure').classList.add('arms');
     } else if(incorrectGuess == 5) {
-        document.querySelector('figure').classList.add('legs')
+        document.querySelector('figure').classList.add('legs');
     } 
 }
 
 // Word array
-const words = ['ord', 'ordet', 'ordetigen'];
-const guessedLetters = [];
+const words = ['array', 'object', 'event', 'class', 'element', 'function', 'variable', 'arrow', 'loop'];
+let guessedLetters = [];
 let guessedLetter;
-const correctWord = randomWord();
-const correctLetters = [];
-let attempts = 0;
+let finalWord = document.querySelector('.words')
+let correctWord = randomWord();
+let correctLetters = [];
 const maxAttempts = 5;
 let incorrectGuess = 0;
+let wrongLetters = [];
+
 
 function randomWord() {
-    const randomInput = Math.floor(Math.random() * (words.length -1));
-    return words[randomInput];
+    const randomInput = Math.floor(Math.random() * (words.length));    
+    const wordCorrect = words[randomInput].toUpperCase();
+    for(let i = 0; i < words[randomInput].length; i++) {
+        let template = 
+        `
+        <article class="correct-letter--container"> 
+        <p class="correct-letter hide">${wordCorrect[i]}</p>
+        </article>
+        `;
+        finalWord.insertAdjacentHTML('beforeend', template);
+    }
+    return words[randomInput].toUpperCase();
 };
 
 const guessElem = document.querySelector('.guess')
 // User letter guess
-window.addEventListener('keypress', (event) => {
-    
-    guessedLetter =  event.key
-    checkGuess(guessedLetter);
-    compareLetters(guessedLetter)
-    guessElem.innerHTML = guessedLetters.join(" ");
-    winGame();
+window.addEventListener('keypress', (event) => {  
+    if(event.key >= 48 || event.key <= 57) {
+        return false;
+    }  
+    guessedLetter = event.key.toUpperCase();
+    checkGuess(guessedLetter);     
 });
 
 // Function to not include the same letter multiple times
 function checkGuess(guessedLetter) {
     if(!guessedLetters.includes(guessedLetter)) {
-        guessedLetters.push(guessedLetter);
-        attempts++;
+        guessedLetters.push(guessedLetter);        
+        compareLetters(guessedLetter)
     } else {
-
         console.log('You have already guessed that letter')
+        return
     }
 };
 
-
-const correctLetterElem = document.querySelector('.correct')
+const testing = document.querySelectorAll('.correct-letter');
+const correctLetterElem = document.querySelector('.correct');
+const wrongElem = document.querySelector('.wrong-letters');
 function compareLetters() {
     for(let i = 0; i < correctWord.length; i++ ) {
-        if(correctWord.includes(guessedLetter)) {
-            correctLetters.push(guessedLetter);
-            correctLetterElem.innerText = correctLetters.join(" ");
-        } else {
-            incorrectGuess++;
-            showHangman()
-        }        
-    }
+        if(correctWord[i].includes(guessedLetter)) {
+            correctLetters.push(guessedLetter);                
+                testing[i].classList.remove('hide');
+        }       
+    } if(!correctWord.includes(guessedLetter)) {
+        incorrectGuess++;
+        wrongLetters.push(guessedLetter)
+        wrongElem.innerHTML = wrongLetters.join(" ");
+        showHangman()
+    }   
+        winGame();     
 };
+
+
 
 function winGame() {
     if(correctLetters.length == correctWord.length) {
-        console.log('You Win!')
-        // overlay.classList.add('.show');
-    } else if(attempts == maxAttempts) {
-        console.log('förlust');
-    } { return }
+        setTimeout(endGame, 1000);
+    } else if(incorrectGuess == maxAttempts) {
+        setTimeout(endGame, 1000);
+    }
 }
+
+let endTemplate;
+const overlayGame = document.querySelector('.overlay')
+function endGame() {
+    if(incorrectGuess >= maxAttempts || timeleft == 0) {
+        endTemplate = 
+        `
+        <p>You suck!</p>
+        <p>The correct word was ${correctWord}.</p>
+        `;
+    } else {
+        endTemplate = 
+        `
+        <p>You won!</p>
+        <p>You had ${incorrectGuess} wrong letters.</p>
+        <p>You had ${timeleft} seconds left!</p>
+        `;
+    }
+     overlayGame.classList.toggle('hidden');
+     overlayGame.insertAdjacentHTML('afterbegin', endTemplate);
+}
+
+let timeleft = 60;
+const gameTimer = setInterval(function(){
+  if(timeleft <= 0){
+    clearInterval(gameTimer);
+    endGame()
+  } else {
+    document.querySelector(".countdown").innerHTML = timeleft + " seconds remaining";
+  }
+  timeleft -= 1;
+}, 1000);
+
+function resetGame() {
+    correctWord = randomWord();
+    correctLetters = [];
+    guessedLetters = [];
+    incorrectGuess = 0;
+    wrongLetters = [];
+    overlayGame.classList.toggle('hidden');
+    finalWord.innerHTML = "";
+    correctWord = randomWord();
+}
+
+const resetButton = document.querySelector('.reset-btn');
+resetButton.addEventListener('click', () => {
+    window.location.reload(); // FUNKAR DET SÅ FUNAKR DET
+    // resetGame()
+})
+
 
